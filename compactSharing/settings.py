f2daf8873d51b1e0b47d8c94ce86669a94860e1b
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-09-26 00:48:00 +0800

    ignore patch version

--- compactSharing/settings.py
+++ compactSharing/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from dotenv import load_dotenv ; load_dotenv()
+from dotenv import load_dotenv; load_dotenv();
 import os
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
