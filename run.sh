78404782cc85c5866a790dfa3dd76b3bf95cb25e
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-10-20 08:35:24 +0000

    move `createsuperuser` into `run.sh`
      - `createsuperuser` needs to be run after `migrate`

--- run.sh
+++ run.sh
@@ -7,9 +7,10 @@ sed -i 's/^URLs =.*/URLs = 0.0.0.0/' .env
 sed -i "s/^DJANGO_SECRET_KEY =.*/DJANGO_SECRET_KEY = $(cat \/dev\/urandom | LC_ALL=C tr -dc a-zA-Z0-9 | head -c 128)/" .env
 popd
 
-# Django init
+# Django init (assume railway)
 uv run -- python manage.py collectstatic --noinput
 uv run -- python manage.py migrate
+uv run -- python manage.py createsuperuser --noinput
 
 # Run with `granian`
 uv run -- granian \
