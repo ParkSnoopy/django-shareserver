5b870c473f99b052992c183bd12c68c4ac91b9c4
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-10-20 07:05:02 +0000

    make `.env` from `.env.example`

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Initialize
-touch .env
-rm .env
-ln -s compactSharing/.env .
+# Environ init
+pushd compactSharing
+cp .env.example .env
+sed -i 's/^URLs =.*/URLs = 0.0.0.0/' .env
+sed -i "s/^DJANGO_SECRET_KEY =.*/DJANGO_SECRET_KEY = $(cat \/dev\/urandom | LC_ALL=C tr -dc a-zA-Z0-9 | head -c 128)/" .env
+popd
+
+# Django init
 uv run -- python manage.py collectstatic --noinput
 uv run -- python manage.py migrate
 
@@ -11,6 +15,6 @@ uv run -- python manage.py migrate
 uv run -- granian \
   --interface wsgi \
   --process-name django-shareserver \
-  --workers 2 \
-  --blocking-threads 8 \
+  --workers 1 \
+  --blocking-threads 4 \
   compactSharing.wsgi:application
