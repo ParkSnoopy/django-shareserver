a6f03f5dcf5da3969bb34f109d3b0b897b69e31e
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-10-20 08:50:58 +0000

    remove CSRF Middleware

--- compactSharing/settings.py
+++ compactSharing/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#always CSRF problem...    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
