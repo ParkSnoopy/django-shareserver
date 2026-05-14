528a39287990ba57337285c77bcac1929ebbec7d
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-05-14 14:12:58 +0800

    fix(31ffabef326209faee8423097eb4d098a7576e44): `reverse` did not have matching name

--- lightfileshare/urls.py
+++ lightfileshare/urls.py
@@ -11,6 +11,6 @@ from .views import (
 urlpatterns = [
     path('', lightfileshare_home), 
     path('details/', lightfileshare_details), 
-    path('download/<int:pk>/', lightfileshare_download),
     path('create/', lightfileshare_create), 
+    path('download/<int:pk>/', lightfileshare_download, name="download"),
 ]
