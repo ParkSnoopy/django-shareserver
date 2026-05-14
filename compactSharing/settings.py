5139124a892d9508e16a798417a3c8e5a78d1ea1
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-05-14 08:36:14 +0000

    lint(all): re-format

--- compactSharing/settings.py
+++ compactSharing/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-
-import os
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
